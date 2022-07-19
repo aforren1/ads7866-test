66b3fab69b3944f3bbe94c314df8b96efd7ac4f4
Alex Forrence <alex.forrence@gmail.com>
2022-07-19 16:28:18 -0400

    working toward parallel input

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,91 @@
-// work out how the ADS7866 works
-// https://www.ti.com/lit/ds/symlink/ads7866.pdf
+/*
+Normal SPI, except all ADCs are hooked up to the same chip select
+and data pins are connected to GPIO6 (pins 14 & 15, respectively)
+
+1. start transaction
+
+*/
 
 /*
-
- Vin   GND  Vdd/ref
-  |     |     |
-            |
-            | <- ref line
-            |
-  |     |     |
-SCLK   SDO    CS
-
-there should be a decoupling (0.1uF) cap between ref and gnd
-
-SCLK determines conversion rate
-
-on teensy 4.0, pin assignments are:
-CS = 10
-SDO (formerly MOSI) = 11
-SDI (formerly MISO) = 12
-SCK (formerly SCK) = 13
-
-max SCK/SCLK = 3.4MHz (at 3.3V ish)
-MSB first
-CPOL=1, CPHA=1, so SPI_MODE3 in arduino lingo
-
-There are no commands, only clocks.
-To start conversion for a single device, pull CS low.
-There will be four 'low' states from device, then 12 bits (1 clock cycle per bit, MSB to LSB)
-After EOC (end of conversion), device's data line pulled to tri-state
-
-Think about flexio for parallel comms, e.g. https://github.com/TeensyUser/doc/wiki/FLEXIO
-
-Total conversion time only takes 5us, so we can still hit 40khz with 5 inputs
-Rise/fall times are 20us (with 1k resistor at 5v), so faster sampling may not make sense?
+TODO: clk_pin not reading
 */
+
 #include <Arduino.h>
 #include <SPI.h>
 
-constexpr int dev1_pin = 10;
+// selects both/all ADCs
+constexpr int cs_pin = 10;
+constexpr int clk_pin = 7;
 
-const SPISettings sets(3400000, MSBFIRST, SPI_MODE3);
+constexpr int d0_pin = 14;
+constexpr int d1_pin = 15;
 
-uint8_t hi, lo;
-uint16_t val;
-uint32_t t0, t1;
+uint16_t counter = 0;
+constexpr uint16_t n_skip = 20; // read once every 20
+uint16_t junk = 0;
+uint16_t val0 = 0;
+uint16_t val1 = 0;
+
+const SPISettings sets(1700000, MSBFIRST, SPI_MODE3);
 
 void setup() {
-  pinMode(dev1_pin, OUTPUT);
-  digitalWriteFast(dev1_pin, HIGH);
+  while (!Serial) {}
+  pinMode(cs_pin, OUTPUT);
+  pinMode(clk_pin, INPUT);
+  // https://forum.pjrc.com/threads/69274-Reading-Pins-in-Parallel-Teensy-4-1?p=298049
+  GPIO6_GDIR &= 0xFFFF; // set all gpio6 pins to input
+  digitalWriteFast(cs_pin, HIGH);
   SPI.begin();
   SPI.beginTransaction(sets);
+  Serial.println("Here");
 }
 
+// completely readable
+// taken from answer and comment from leoly
+// https://stackoverflow.com/questions/47981/how-do-i-set-clear-and-toggle-a-single-bit#comment46654671_47990
+inline uint16_t set_bit_at_pos(uint16_t val, uint8_t gpio_val, uint8_t pos) {
+    return val & ~(1 << pos) | (gpio_val << pos);
+}
+
+elapsedMicros tm;
+
 void loop() {
-  delay(1);
-  t0 = micros();
-  digitalWriteFast(dev1_pin, LOW);
-  hi = SPI.transfer(0);
-  lo = SPI.transfer(0);
-  digitalWriteFast(dev1_pin, HIGH);
-  t1 = micros();
-  val = (hi << 8) | (lo & 0xff);
-  Serial.print(val);
-  Serial.print(" ");
-  Serial.println(t1 - t0);
+    Serial.println(counter);
+    tm = 0;
+    // start reading
+    digitalWriteFast(cs_pin, LOW);
+    if (counter == 0) {
+        // watch the clock. We're going to manually
+        // read the data out of GPIO6 after every clock cycle
+        for (uint16_t i = 0; i < 16; i++) {
+            // spin until low
+            Serial.println(i);
+            while (digitalReadFast(clk_pin)) {}
+            // data valid? now read all of GPIO6, and shift out the bits we need
+            register uint32_t data = GPIO6_PSR;
+            val0 = set_bit_at_pos(val0, (data >> 18) & 1U, i); // pin 14
+            val1 = set_bit_at_pos(val1, (data >> 19) & 1U, i); // pin 15
+            // spin until high
+            while (!digitalReadFast(clk_pin)) {}
+        }
+
+    } else {
+        // read, but we're going to ignore the value
+        // idea is to force the ADC to do the work, which
+        // should spread out noise
+        // we only really want ~1kHz, and we're getting 200kHz
+        junk = SPI.transfer16(0);
+    }
+
+    // stop reading
+    digitalWriteFast(cs_pin, HIGH);
+    Serial.println(tm);
+    // send data
+    if (counter == 0) {
+        Serial.print(val0);
+        Serial.print(" ");
+        Serial.println(val1);
+    }
+    counter += 1;
+    counter %= n_skip;
 }
